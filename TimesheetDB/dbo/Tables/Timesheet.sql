b2168f6d7f4a692ace5f134234a3f392f8210536
afzal-hussain259 <afi259@hotmail.co.uk>
2025-02-10 21:42:10 +0000

    Database project created and sql table created to store timesheet data.

---
+++ TimesheetDB/dbo/Tables/Timesheet.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Timesheet]
+(
+	[Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]           NVARCHAR (50)  NOT NULL,
+    [Date]               DATE           NOT NULL,
+    [Project]            NVARCHAR (50)  NOT NULL,
+    [DescriptionOfTasks] NVARCHAR (MAX) NOT NULL,
+    [HoursWorked]        DECIMAL (18)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
